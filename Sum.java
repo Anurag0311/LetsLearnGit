49dfc41ed856d55e4a5ff64ea166a511ec57b30b
Anurag <anuragsingh.anurag1234@gmail.com>
2023-07-13 13:19:32 +0530

    added message in Sum.java

--- Sum.java
+++ Sum.java
@@ -2,6 +2,6 @@ public class Sum{
     public static void main(String [] args){
         int a=6;
         int b=5;
-        System.out.println(a+b);
+        System.out.println("Sum of a and b:"+(a+b));
     }
 }
